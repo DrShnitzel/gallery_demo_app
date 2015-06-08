71f59708d7ce28dedd8c0b2bbe092a76726eda8f
Alexey Zaitsev <zaitsev.av.work@gmail.com>
2015-06-08 11:35:22 +0300

    Switch to rubinius in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'rest_test_task'
 set :repo_url, 'https://github.com/DrShnitzel/gallery_demo_app.git'
 
-set :rbenv_ruby, '2.2.1'
+set :rbenv_ruby, 'rbx-2.5.2'
 set :linked_files, %w(.env) + (fetch(:linked_files) || [])
 set :deploy_to, '/var/www/rest_test_task'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
